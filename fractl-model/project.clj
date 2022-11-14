a69de24ab5195b196900fcc2a33744d49aa94ec0
Vijay Mathew <vijay@fractl.io>
2022-11-14 13:24:17 +0530

    handle kebab-case app-name

--- fractl-model/project.clj
+++ fractl-model/project.clj
@@ -1,3 +1,4 @@
 (defproject fractl-model/lein-template "0.1.0"
   :description "Generate fractl-model template projects"
+  :dependencies [[camel-snake-kebab "0.4.3"]]
   :eval-in-leiningen true)
--- fractl-model/src/leiningen/new/fractl_model.clj
+++ fractl-model/src/leiningen/new/fractl_model.clj
@@ -1,20 +1,31 @@
 (ns leiningen.new.fractl-model
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :as main]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [camel-snake-kebab.core :as csk]))
 
 (def render (renderer "fractl-model"))
 
-(defn- parse-name [app-name]
-  (s/split app-name #":"))
+(defn- parse-name [n]
+  (s/split n #":"))
+
+(def ^:private app-name first)
+(def ^:private app-version second)
+
+(defn- capsfirst [s]
+  (str (s/upper-case (first s)) (apply str (rest s))))
+
+(defn- normalize-component-name [n]
+  (keyword (capsfirst (csk/->camelCase (name n)))))
 
 (defn fractl-model
-  [app-name]
-  (let [xs (parse-name app-name)
-        data {:name (first xs)
-              :version (or (second xs) "0.0.1")
-              :component-name (keyword (s/capitalize (name app-name)))
-              :sanitized (name-to-path app-name)}]
+  [app-info]
+  (let [xs (parse-name app-info)
+        n (app-name xs)
+        data {:name n
+              :version (or (app-version xs) "0.0.1")
+              :component-name (normalize-component-name n)
+              :sanitized (name-to-path n)}]
     (main/info "Generating fresh 'lein new' fractl-model project.")
     (->files data
              ["README.md" (render "README.md" data)]
